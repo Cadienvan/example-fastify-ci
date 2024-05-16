9acbee8c29094e49d03f6feafdeaad5045e0a9ea
Michael Di Prisco <cadienvan@gmail.com>
2024-05-16 10:13:18 +0200

    ci: fixing ci

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import { fastify } from './http/index.js';
 
 console.log('Waiting for connections...');
-await Promise.all([fastify.listen({ port: 3000 })]);
+await Promise.all([fastify.listen({ host: '0.0.0.0', port: 3000 })]);
 console.log('Connections established!');
--- how-to-use-it.md
+++ how-to-use-it.md
@@ -1,2 +1,2 @@
 docker pull ghcr.io/cadienvan/example-fastify-ci:main
-docker run --rm --platform linux/arm64/v8 ghcr.io/cadienvan/example-fastify-ci:main -i
+docker run --rm --platform linux/arm64/v8 -i -p 127.0.0.1:15001:3000 ghcr.io/cadienvan/example-fastify-ci:main
